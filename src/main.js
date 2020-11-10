88f89757a858a2ed073b178a345477d1395169da
Brian Harvey <brian.harv3y@gmail.com>
2020-11-10 10:14:51 -0800

    initial commit
    Co-authored-by: Vanessa Stewart <vstewart1213@gmail.com>

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import 'plant.js'
+
+
+$(document).ready(function() {
+
+  $('#feed').click(function() {
+    const newState = stateControl(bluefood);
+    $('#soil-value').text(`Soil: ${newState.soil}`);
+  });
+
+  $('#show-state').click(function() {
+    const currentState = stateControl();
+    $('#soil-value').text(`Soil: ${currentState.soil}`);
+  });
+});
+
---
+++ src/plant.js
@@ -0,0 +1,37 @@
+
+
+const storeState = (initialState) => {
+  let currentState = initialState;
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = {...newState};
+    return newState;
+  }
+}
+
+const stateControl = storeState();
+
+
+const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop] : (state[prop] || 0) + value
+    })
+  }
+}
+
+
+
+const feed = changeState("soil");
+const hydrate = changeState("water");
+const giveLight = changeState("light");
+
+
+const blueFood = changeState("soil")(5);
+const greenFood = changeState("soil")(10);
+const superWater = changeState("water")(5);
+
+
+
+
